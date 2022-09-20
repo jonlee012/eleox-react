11b0d3f5227a57ab3b8e428f71c287c775e5abd0
jonlee012 <jonlee86.jl@gmail.com>
2022-09-20 14:09:27 -0500

    login form functionality created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+// import logo from './logo1.png';
+// import {Routes, Route, useNavigate} from 'react-router-dom';
 import './App.css';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+
+  const adminUser= {
+  username: "interview",
+    password: "eleox"
+  }
+
+  const [user, setUser] = useState({name:"", username:""});
+  const [error, setError] = useState("");
+
+  const Login = details => {
+    console.log(details);
+    if (details.username == adminUser.username && details.password == adminUser.password){
+      console.log("logged in");
+      setUser({
+        name: details.name,
+        username: details.username
+      })
+    } else{
+      console.log("details do not match");
+      alert("incorrect credentials")
+    }
+  }
+
+  const Logout = () => {
+    setUser({name:"", username:""});
+    }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.username != "") ? (
+          <div className="welcome">
+            <h2>Welcome, <span>{user.name}</span></h2>
+            <button onClick={Logout}>Logout</button>
+          </div>
+      ) : (
+         <LoginForm Login={Login} error={error} />
+      )}
+
+
+
     </div>
   );
 }
