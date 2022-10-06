c60a1bdf837d25f4e4fdfbeb1228f4704ecb1dea
jonlee012 <jonlee86.jl@gmail.com>
2022-10-05 22:26:07 -0500

    update to login page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,10 +6,32 @@ const HomePage = () => {
     <div className="text-center">
         <h1 className="main-title home-page-title">welcome</h1>
         <Link to="/">
-            <button className="primary-button">Log out</button>
+            <button to="/login" className="primary-button">Log out</button>
         </Link>
     </div>
 )
 };
 
+
+const Home = () => {
+    return (
+        <div>
+           
+                <Avatar image={Logo}/>
+            </div>
+            <StyledTitle size={65}>
+                Welcome to Eleox
+            </StyledTitle>
+            <StyledSubTitle size={27}>
+                Feel free to explore our page
+            </StyledSubTitle>
+
+            <ButtonGroup><StyledButton to="/login">Login</StyledButton>
+                <StyledButton to="/signup">Signup</StyledButton>
+            </ButtonGroup>
+    
+    )
+};
+
+export default Home;
 export default HomePage;
