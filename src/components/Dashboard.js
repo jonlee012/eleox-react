a2310ab848a8eff2d312df7a557308318ed6dddb
jonlee012 <jonlee86.jl@gmail.com>
2022-10-05 22:25:56 -0500

    update to login page

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,23 +1,14 @@
-import { useEffect, useState } from "react";
-import React from 'react';
-import { getUser, removeUserSession } from './Utils/Common';
+import React from "react";
 
 const Dashboard = () => {
-    const user = getUser();
-    const [authenticated, setauthenticated] = useState(null);
-    useEffect(() => {
-        const loggedInUser = localStorage.getItem("authenticated");
-        if (loggedInUser) {
-            setauthenticated(loggedInUser);
-        }
-    }, []);
-    if (!authenticated) {
-        // Redirect
-    } else {
+
         return (
             <div>
                 <p>Welcome to your Dashboard</p>
             </div>
+    <Link to="/logout">
+        <button className="primary-button">log out</button>
+    </Link>
         );
     }
 };
